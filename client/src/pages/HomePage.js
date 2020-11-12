b74c069850ba1cbbffed4253dd108686090521c0
Christina <christinalu42@gmail.com>
2020-11-11 17:04:32 -0700

    created styles folder. install react bootstrap

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div className="page">
+        
+      </div>
+    );
+  }
+
+}
+
+export default HomePage;
