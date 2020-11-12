df069777f5de16fe41ee0c647783855a2a7a7245
Christina <christinalu42@gmail.com>
2020-11-11 17:15:39 -0700

    import bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 import { Route, Switch, BrowserRouter } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
