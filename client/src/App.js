ff504a62084659a09b44e52811cb6507970db27c
Justin Flores <justin.flores@ucalgary.ca>
2020-11-19 23:31:45 -0700

    Setup Protected Route and refactored authentication flow

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProtectedRoute from "./Utils/ProtectedRoute";
 
 export default class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
-          <HomePage />
-        </div>
+        <Switch>
+          <div className="App">
+            <Route exact path="/login">
+              <LoginPage />
+            </Route>
+
+            <ProtectedRoute exact path="/" component={HomePage} />
+          </div>
+        </Switch>
       </Router>
     );
   }
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,56 +1,30 @@
-import React from 'react';
-import LoginPage from './LoginPage';
-import '../styles/HomePage.css'
-import Accordion from 'react-bootstrap/Accordion';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import InputGroup from 'react-bootstrap/InputGroup'
-import FormControl from 'react-bootstrap/FormControl'
-import Toast from 'react-bootstrap/Toast'
+import React from "react";
+import "../styles/HomePage.css";
+import {
+  Accordion,
+  Card,
+  InputGroup,
+  FormControl,
+  Toast,
+  Button,
+} from "react-bootstrap";
 
-export default class HomePage extends React.Component {
+import AuthContext from "../context/AuthContext";
+
+class HomePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       gameCode: "",
       showRoomNotFound: false,
-      user: {},
-      error: null,
-      authenticated: false,
-    }
+    };
     this.handleGameCodeChange = this.handleGameCodeChange.bind(this);
     this.handleJoinGameClicked = this.handleJoinGameClicked.bind(this);
     this.handleLogoutClick = this.handleLogoutClick.bind(this);
     this.handleCreateGameClicked = this.handleCreateGameClicked.bind(this);
   }
 
-  componentDidMount() {
-    fetch("http://localhost:8888/auth/login/success", {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Credentials": true,
-      },
-    })
-      .then((response) => {
-        if (response.status === 200) return response.json();
-        throw new Error("failed to authenticate user");
-      })
-      .then((responseJson) => {
-        this.setState({
-          authenticated: true,
-          user: responseJson.user,
-        });
-      })
-      .catch((error) => {
-        this.setState({
-          authenticated: false,
-          error: "Failed to authenticate user",
-        });
-      });
-  }
+  componentDidMount() {}
 
   handleLogoutClick() {
     window.open("http://localhost:8888/auth/logout", "_self");
@@ -63,113 +37,139 @@ export default class HomePage extends React.Component {
     // TODO: redirect to match history page
   }
 
-  handleViewProfileClicked(){
+  handleViewProfileClicked() {
     // TODO: redirect to profile page
   }
 
   handleCreateGameClicked() {
-    fetch(`http://localhost:8888/lobby/create?hostId=${this.state.user.id}&hostName=${this.state.user.name}`, {
-      method: "POST",
-      // credentials: "include",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        // "Access-Control-Allow-Credentials": true,
-      },
-    })
-    .then((response) => {
-      if (response.status === 200) return response.json();
-      throw new Error("failed create new room");
-    })
-    .then((responseJson) => {
-      console.log(responseJson)
-      // redirect to lobby
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    const user = this.props.authCreds.auth.user;
+    fetch(
+      `http://localhost:8888/lobby/create?hostId=${user.id}&hostName=${user.name}`,
+      {
+        method: "POST",
+        // credentials: "include",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          // "Access-Control-Allow-Credentials": true,
+        },
+      }
+    )
+      .then((response) => {
+        if (response.status === 200) return response.json();
+        throw new Error("failed create new room");
+      })
+      .then((responseJson) => {
+        console.log(responseJson);
+        // redirect to lobby
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   handleJoinGameClicked() {
     if (this.state.gameCode === "") return;
-      // TODO: if game exists, redirect them to game else do below
-      this.setState({
-        showRoomNotFound: true,
-      });
+    // TODO: if game exists, redirect them to game else do below
+    this.setState({
+      showRoomNotFound: true,
+    });
   }
 
   handleGameCodeChange(e) {
     this.setState({
-      gameCode: e.target.value  
-    })
+      gameCode: e.target.value,
+    });
   }
 
-  
-
   render() {
-    const { authenticated } = this.state;
     return (
-      <div className='page'>
-        {!authenticated ? 
-          <LoginPage />
-        : 
-          <div className='content'>
-            <h3>{this.props.username || "kirby placeholder"}</h3>
-            <img className="profile-picture" src="https://play.nintendo.com/images/profile-kirby-kirby.7bf2a8f2.aead314d58b63e27.png" alt="pfp"></img>
+      <div className="page">
+        <div className="content">
+          <h3>{this.props.authCreds.auth.user.name || "kirby placeholder"}</h3>
+          <img
+            className="profile-picture"
+            src="https://play.nintendo.com/images/profile-kirby-kirby.7bf2a8f2.aead314d58b63e27.png"
+            alt="pfp"
+          ></img>
 
-            <div className='button-group'>
-              <Button variant="outline-danger" onClick={this.handleLogoutClick}>Logout</Button>
-              <Button variant="outline-dark" onClick={this.handleMatchHistoryClicked}>Match History</Button>{' '}
-              <Button variant="outline-dark" onClick={this.handleViewProfileClicked}>View Profile</Button>{' '}
-              <Button variant="info" onClick={this.handleCreateGameClicked}>Create Game</Button>{' '}
-            </div>
-
-            <InputGroup style={{ maxWidth: '70%', margin: '10px auto' }}>
-              <FormControl
-                placeholder="Enter lobby code to join a game"
-                value={this.state.gameCode}
-                onChange={this.handleGameCodeChange}
-                aria-label="Lobby Code"
-              />
-              <InputGroup.Append>
-                <Button variant="success" onClick={this.handleJoinGameClicked}>Join Game</Button>
-              </InputGroup.Append>
-            </InputGroup>
-
-            <Toast 
-              onClose={() => this.setState({showRoomNotFound: false})} 
-              show={this.state.showRoomNotFound} 
-              delay={3000} autohide
-              style={{ maxHeight: 50}}
+          <div className="button-group">
+            <Button
+              variant="outline-danger"
+              onClick={this.props.authCreds.logout}
             >
-              <Toast.Body>Couldn't find the room you're looking for. Sorry!</Toast.Body>
-            </Toast>
-
-            <Accordion className='info'>
-              <Card>
-                <Card.Header>
-                  <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                    How to Play
-                  </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body>
-                    Create a game lobby and invite your friends to play or join 
-                    an existing lobby above. Adjust the settings to your liking 
-                    and start the game. When its your turn to draw, you will have 
-                    to choose one of the three words that appear and visualize 
-                    that word in a set amount of time. The more people that get 
-                    the word right, the more points you win! When somebody else 
-                    is drawing you have to type your guess into the chat to gain 
-                    points, be quick, the earlier you guess a word the more points 
-                    you get! Tip: hints will appear above the canvas as time passes.
-                  </Card.Body>
-                </Accordion.Collapse>
-              </Card>
-            </Accordion>
+              Logout
+            </Button>
+            <Button
+              variant="outline-dark"
+              onClick={this.handleMatchHistoryClicked}
+            >
+              Match History
+            </Button>{" "}
+            <Button
+              variant="outline-dark"
+              onClick={this.handleViewProfileClicked}
+            >
+              View Profile
+            </Button>{" "}
+            <Button variant="info" onClick={this.handleCreateGameClicked}>
+              Create Game
+            </Button>{" "}
           </div>
-        }
+
+          <InputGroup style={{ maxWidth: "70%", margin: "10px auto" }}>
+            <FormControl
+              placeholder="Enter lobby code to join a game"
+              value={this.state.gameCode}
+              onChange={this.handleGameCodeChange}
+              aria-label="Lobby Code"
+            />
+            <InputGroup.Append>
+              <Button variant="success" onClick={this.handleJoinGameClicked}>
+                Join Game
+              </Button>
+            </InputGroup.Append>
+          </InputGroup>
+
+          <Toast
+            onClose={() => this.setState({ showRoomNotFound: false })}
+            show={this.state.showRoomNotFound}
+            delay={3000}
+            autohide
+            style={{ maxHeight: 50 }}
+          >
+            <Toast.Body>
+              Couldn't find the room you're looking for. Sorry!
+            </Toast.Body>
+          </Toast>
+
+          <Accordion className="info">
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                  How to Play
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  Create a game lobby and invite your friends to play or join an
+                  existing lobby above. Adjust the settings to your liking and
+                  start the game. When its your turn to draw, you will have to
+                  choose one of the three words that appear and visualize that
+                  word in a set amount of time. The more people that get the
+                  word right, the more points you win! When somebody else is
+                  drawing you have to type your guess into the chat to gain
+                  points, be quick, the earlier you guess a word the more points
+                  you get! Tip: hints will appear above the canvas as time
+                  passes.
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
+        </div>
       </div>
     );
   }
 }
+
+export default AuthContext(HomePage);
