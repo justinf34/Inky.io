b2b40349d9a15c5df091f015369c84fefd44af62
Dheeraj <dheeraj.kumar@ucalgary.ca>
2020-12-13 12:56:12 -0700

    minor fix on the leave button

--- client/src/pages/GameLobbyPage.js
+++ client/src/pages/GameLobbyPage.js
@@ -16,7 +16,6 @@ class GameLobbyPage extends React.Component {
       roomLink: this.props.match.params.lobbyID,
       numRoundsOptions: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
       drawingTimeOptions: [
-        5,
         30,
         45,
         60,
